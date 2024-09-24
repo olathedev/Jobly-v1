afda00fcc114d65688c1566aacd1480705d2e706
olathedev <olapademicheal3@gmail.com>
2024-09-24 10:44:10 +0100

    added health endpoint and deployed to render

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(helmet())
 app.use(cors())
 app.use(xssClean())
 app.use(rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 }))
 
 const start = async () => {
@@ -43,11 +43,18 @@ const start = async () => {
 
 start()
 
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        message: "Server is up and grateful 🙏📿",
+    })
+})
+
+
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/jobs', auth, jobROutes)
 
 
-app.use(notFOund)   
+app.use(notFOund)
 app.use(errorHandler)
 
 
